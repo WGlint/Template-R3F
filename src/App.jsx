a5546d408dd7e76f0f4c26434a4653e4e06be091
WGlint <a.elmediri@gmail.com>
2023-04-06 22:14:27 +0200

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { OrbitControls, Sky } from '@react-three/drei'
+import { BlendFunction, GlitchMode } from 'postprocessing'
+import { useControls } from 'leva';
+import { useRef } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+
+
+export default function App()
+{
+
+    return <>
+    
+        <OrbitControls />
+        <ambientLight/>
+        <directionalLight position={[ 1,1,1 ]} />
+
+        <mesh castShadow position-x={ - 2 }>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+        </mesh>
+
+        <mesh>
+            <boxGeometry/>
+            <meshStandardMaterial color="red" />
+        </mesh>
+
+        <mesh scale={100} rotation-x={-Math.PI/2} position-y={-1} >
+            <planeGeometry/>
+            <meshStandardMaterial color="green" />
+        </mesh>
+
+    </>
 }
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './main.scss'
+import { Canvas } from '@react-three/fiber'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Canvas>
+        <App />
+
+        <color attach="background" args={[ '#201919' ]} />
+    </Canvas>
   </React.StrictMode>,
 )
